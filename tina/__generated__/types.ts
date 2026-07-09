13493ffc8e5cc02ccbb51bdd9a51ee2762bd35b8
Afzal Ansari <afzal@orgzit.com>
2026-07-09 14:46:42 +0530

    "fixed"

--- tina/__generated__/types.ts
+++ tina/__generated__/types.ts
@@ -504,7 +504,7 @@ export const ExperimentalGetTinaClient = () =>
   getSdk(
     generateRequester(
       createClient({
-        url: "https://content.tinajs.io/2.4/content/9dd68aa3-cac3-48a2-a4aa-2b88e58846e2/github/main",
+        url: "http://localhost:4001/graphql",
         queries,
       })
     )
